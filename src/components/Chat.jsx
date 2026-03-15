58e08871cbd76b8c48a94a81b2df4753bd5b05af
gem-ManishNibandhe <manunibandhe5@gmail.com>
2026-03-15 17:31:49 +0530

    fix

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -49,7 +49,7 @@ export const Chat = () => {
         
     // emit to server
     console.log('Emitting sendMessage with payload:', { firstName: user?.firstName, lastName: user?.lastName, userId, targetUserId, text: newMessage });
-    // socket.emit('sendMessage', { firstName: user?.firstName, lastName: user?.lastName, userId, targetUserId, text: newMessage });
+    socket.emit('sendMessage', { firstName: user?.firstName, lastName: user?.lastName, userId, targetUserId, text: newMessage });
         setNewMessage('')
         
     }
@@ -63,8 +63,6 @@ export const Chat = () => {
         }
         
         const socket = createSocketConnection();
-
-        console.log('Joining chat with:', { firstName: user?.firstName, userId, targetUserId });
 
         // join the chat room for this pair
         socket.emit('joinChat', { firstName: user?.firstName, userId, targetUserId });
