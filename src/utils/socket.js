f5f7084b60aa30c8ba638b0581019a5a80ba6094
gem-ManishNibandhe <manunibandhe5@gmail.com>
2026-03-15 16:55:34 +0530

    socket bug fix

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -5,16 +5,30 @@ let socket = null;
 
 export const createSocketConnection = () => {
     if (!socket) {
-        if(location.hostname === "localhost"){
+        if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
             socket = io(BASE_URL, {
                 withCredentials: true,
             });
+        } else {
+            // production: assume nginx proxies /api to backend and socket path is /api/socket.io
+            socket = io('/', { path: '/api/socket.io', withCredentials: true });
         }
-        else{
-            // for production we need to specify the path as well because of nginx configuration   
-        socket = io('/',{path: '/api/socket.io'})   
+
+        socket.on('connect', () => {
+            console.log('Socket connected', socket.id);
+        });
+
+        socket.on('disconnect', (reason) => {
+            console.log('Socket disconnected', reason);
+        });
     }
 
     return socket;
 }
+
+export const disconnectSocket = () => {
+    if (socket) {
+        socket.disconnect();
+        socket = null;
+    }
 }
